eb17ddf417e8ba8dd01b90d54ed24b30d91f9337
JarbasSantosSilva <jarbas7santos@gmail.com_JarbasSantosSilva>
2026-07-28 05:42:42 -0300

    adiciona POST, PUT a rota matricula

--- routes/matricula_routes.py
+++ routes/matricula_routes.py
@@ -32,3 +32,82 @@ def listar_matriculas():
             cursor.close()
         if conn:
             conn.close()
+
+@matricula_bp.post('/')
+def criar_matricula():
+    conn = None
+    cursor = None
+    try:
+        dados = request.get_json()
+        if not dados:
+            return jsonify({"Erro":"Nenhum dado encontrado."}),400
+        if "aluno_id" not in dados or "curso_id" not in dados or "data_matricula" not in dados:
+            return jsonify({"Erro":"Dados enviados insuficientes."}),400
+        aluno_id = dados.get("aluno_id")
+        curso_id = dados.get("curso_id")
+        data_matricula = dados.get("data_matricula")
+
+        conn = obter_conexao()
+        cursor = conn.cursor()
+        cursor.execute("insert into matriculas (aluno_id, curso_id, data_matricula) values (%s, %s, %s)",(aluno_id, curso_id, data_matricula))
+
+        conn.commit()
+        return jsonify({"Mensagem": "Matricula criada."}),201
+
+    except mysql.connector.Error as erro_banco:
+        if conn:
+            conn.rollback()
+        return jsonify({"Erro":f"Erro com o banco de dados : {erro_banco} ."}),500
+    except Exception as erro:
+        if conn:
+            conn.rollback()
+        return jsonify({"Erro":f"Erro inesperado : {erro} ."}),500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+@matricula_bp.put('/<int:id>')
+def atualizar_matricula(id):
+    conn = None
+    cursor = None
+    try:
+        dados = request.get_json()
+        if not dados:
+            return jsonify({"Erro":"Dados não enviados."}),400
+        if "aluno_id" not in dados or "curso_id" not in dados or "data_matricula" not in dados:
+            return jsonify({"Erro":"Dados enviados insuficientes."}),400
+        
+        novo_aluno_id = dados.get("aluno_id")
+        novo_curso_id = dados.get("curso_id")
+        nova_data_matricula = dados.get("data_matricula")
+
+        conn = obter_conexao()
+        cursor = conn.cursor()
+        cursor.execute("update matriculas set aluno_id = %s, curso_id = %s, data_matricula = %s where id = %s",(novo_aluno_id, novo_curso_id, nova_data_matricula, id))
+
+        if cursor.rowcount == 0:
+            return jsonify({"Erro": "Matricula inexistente."}),400
+        conn.commit()
+        return jsonify({"Mensagem": "Matricula atualizada."}),200
+
+    except mysql.connector.Error as erro_banco:
+        if conn:
+            conn.rollback()
+        return jsonify({"Erro": f"Erro com o banco de dados : {erro_banco} ."}),500
+    except Exception as erro:
+        if conn:
+            conn.rollback()
+        return jsonify({"Erro":f"Erro inesperado : {erro}"}),500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+
+
+
+
+
